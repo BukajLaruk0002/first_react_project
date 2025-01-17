756a491af444cb04488c49b0c9b72f4f8127b40c
BukajLaruk0002 <bukajlaruk@gmail.com>
2025-01-17 11:47:51 +0100

    1

--- first_project/src/App.jsx
+++ first_project/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import './test.jsx'
+import Images from './test.jsx'
 /*
 const Images = () => (
   <div>
