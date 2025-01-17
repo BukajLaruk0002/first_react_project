68487694c68d97d10f3ad5de5ca04eef2a9ea248
jakub kural <maszpoczestujsie@wp.pl>
2025-01-17 12:49:47 +0100

    ff

--- first_project/src/Button.jsx
+++ first_project/src/Button.jsx
@@ -1,3 +1,9 @@
-const Button = (props) => <button>{props.children}</button>;
+const Button = ({color, title='Example title', children}) => 
+<button>
+<p>
+    {title}
+</p>
+{children}
+</button>;
 
 export default Button;    
