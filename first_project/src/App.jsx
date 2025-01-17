780671be9ca60c5be5ac296095ae42f872ce8e48
BukajLaruk0002 <bukajlaruk@gmail.com>
2025-01-17 11:47:04 +0100

    3

--- first_project/src/App.jsx
+++ first_project/src/App.jsx
@@ -14,6 +14,7 @@ const Images = () => (
     </a>
   </div>
 );
+*/
 function App() {
   const [count, setCount] = useState(0)
 
@@ -28,8 +29,7 @@ function App() {
       </div>
       
     </>
-  )
+  );
 }
 
-export default App
-*/
+export default App;
--- first_project/src/test.jsx
+++ first_project/src/test.jsx
@@ -13,21 +13,5 @@ const Images = () => (
     </a>
   </div>
 );
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <Images/>
-      <div>
-      
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-      </div>
-      
-    </>
-  )
-}
-
-export default App
+export default Images;
