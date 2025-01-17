8b26f106dd1152b234e1aa5d0ad8dd9ffbef79bf
jakub kural <maszpoczestujsie@wp.pl>
2025-01-17 14:47:53 +0100

    popopopo

--- first_project/src/App.jsx
+++ first_project/src/App.jsx
@@ -16,9 +16,9 @@ function App() {
       <Images/>
       <h1>Vite + React</h1>
       <div className='buttons'>
-        <Button onClick={() => changeDESC("first")}>first</Button>
-        <Button onClick={() => changeDESC("second")}>second</Button>
-        <Button onClick={() => changeDESC("third")}>third</Button>
+        <Button onClick={() => changeDESC("first")} isActive={description === "first"}>first</Button>
+        <Button onClick={() => changeDESC("second")} isActive={description === "second"}>second</Button>
+        <Button onClick={() => changeDESC("third")} isActive={description === "third"}>third</Button>
       </div>
       <div className="card">
         <Opis>{opisy[description]}</Opis>
