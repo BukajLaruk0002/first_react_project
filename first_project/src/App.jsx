084be64ea33e68eeb2192eea89dafeb147272ab9
jakub kural <maszpoczestujsie@wp.pl>
2025-01-17 14:04:19 +0100

    er

--- first_project/src/App.jsx
+++ first_project/src/App.jsx
@@ -5,7 +5,7 @@ import Opis from './opis.jsx'
 import opisy from './Opisy.js'
 
 function App() {
-const clickHandler = (text) => {
+  const changeDESC = (text) => {
   console.log(text);
 };
   return (
@@ -14,7 +14,7 @@ const clickHandler = (text) => {
       <h1>Vite + React</h1>
       <div className='buttons'>
         <Button onClick={() => {console.log("first button")}}>first</Button>
-        <Button onClick={() => clickHandler("second button")}>second</Button>
+        <Button onClick={() => changeDESC("second button")}>second</Button>
         <Button onClick={() => {console.log("third button")}}>third</Button>
       </div>
       <div className="card">
