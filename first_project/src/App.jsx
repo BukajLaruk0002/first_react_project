34ca843e6004e6d28139f13a69634e751886bda0
BukajLaruk0002 <bukajlaruk@gmail.com>
2025-01-17 11:38:41 +0100

    1

--- first_project/src/App.jsx
+++ first_project/src/App.jsx
@@ -2,7 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+const Images = () => (
+  <div>
+    <a href="https://vite.dev" target="_blank">
+    <img src={viteLogo} className="logo" alt="Vite logo" />
+    </a>
+    <a href="https://react.dev" target="_blank">
+    <img src={reactLogo} className="logo react" alt="React logo" />
+    </a>
+  </div>
+);
 function App() {
   const [count, setCount] = useState(0)
 
