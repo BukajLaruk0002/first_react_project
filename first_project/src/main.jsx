42385574101ce092e60144913b27c74d22e8c0b3
jakub kural <maszpoczestujsie@wp.pl>
2025-01-20 13:14:16 +0100

    chakra operational

--- first_project/src/main.jsx
+++ first_project/src/main.jsx
@@ -1,4 +1,4 @@
-import { Provider } from "../components/ui/Provider"
+import { ChakraProvider } from '@chakra-ui/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
