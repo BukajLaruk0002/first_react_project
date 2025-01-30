4dda661d77686577e2613f7b12b3d540fca25cf6
jakub kural <maszpoczestujsie@wp.pl>
2025-01-30 12:16:52 +0100

    dfs

--- first_project/src/main.jsx
+++ first_project/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "@/components/ui/provider";
+import { Provider } from "./components/ui/provider";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { SWRConfig } from "swr";
 import axios from "axios";
@@ -9,6 +9,7 @@ import Layout from "./components/Layout/index.jsx";
 import Home from "./routes/Home.jsx";
 import Categories from "./routes/Categories.jsx";
 import Category from "./routes/Category.jsx";
+import Product from "./routes/Product.jsx";
 import Account from "./routes/Account.jsx";
 import fetcher from "./fetcher.js";
 
@@ -24,6 +25,7 @@ createRoot(document.getElementById("root")).render(
               <Route index element={<Home />} />
               <Route path="/categories" element={<Categories />} />
               <Route path="/categories/:category" element={<Category />} />
+              <Route path="/product/:id" element={<Product />} />
               <Route path="/account" element={<Account />} />
             </Route>
           </Routes>
--- first_project/src/routes/Category.jsx
+++ first_project/src/routes/Category.jsx
@@ -1,16 +1,8 @@
 import { useParams } from "react-router";
 import useSWR from "swr";
-import {
-  Flex,
-  Spinner,
-  Center,
-  Alert,
-  Text,
-  Card,
-  Image,
-  Button,
-  SimpleGrid,
-} from "@chakra-ui/react";
+import { Flex, Spinner, Center, Alert, SimpleGrid } from "@chakra-ui/react";
+
+import ProductCard from "../ProductCard";
 
 const Category = () => {
   const { category } = useParams();
@@ -35,50 +27,7 @@ const Category = () => {
 
         <SimpleGrid columns={3} gap={8}>
           {categoryData?.map((product) => (
-            <Card.Root
-              key={product.id}
-              maxW="sm"
-              overflow="hidden"
-              backgroundColor={"white"}
-              cursor={"pointer"}
-            >
-              <Image
-                src={product.image}
-                alt={product.title}
-                width={400}
-                height={200}
-                objectFit={"contain"}
-                padding={2}
-              />
-              <Card.Body gap="2" backgroundColor="whitesmoke">
-                <Card.Title color="black">{product.title}</Card.Title>
-                <Card.Description color="black">
-                  {product.description}
-                </Card.Description>
-                <Text
-                  textStyle="2xl"
-                  fontWeight="medium"
-                  letterSpacing="tight"
-                  mt="2"
-                  color="black"
-                >
-                  {`$${product.price}`}
-                </Text>
-              </Card.Body>
-              <Card.Footer
-                gap="2"
-                backgroundColor="gray.300"
-                paddingTop={5}
-                cursor={"default"}
-              >
-                <Button variant="solid" colorPalette={"green"}>
-                  Buy now
-                </Button>
-                <Button variant="subtle" colorPalette={"blue"}>
-                  Add to cart
-                </Button>
-              </Card.Footer>
-            </Card.Root>
+            <ProductCard key={product.id} product={product} />
           ))}
         </SimpleGrid>
       </Center>
