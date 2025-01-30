0eb46e17293f6330e3059aafa182694db46b6b5d
jakub kural <maszpoczestujsie@wp.pl>
2025-01-30 14:37:49 +0100

    xxcx

--- first_project/src/routes/Account.jsx
+++ first_project/src/routes/Account.jsx
@@ -9,11 +9,25 @@ import {
 } from "@chakra-ui/react";
 import { Eye, EyeOff } from "lucide-react";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { data } from "react-router";
+import { validator } from "validator";
 
 const Account = () => {
   const [showPassword, setShowPassword] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { error },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <Center>
+    <Center width={"100%"}>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate />
       <Flex
         gap={5}
         justifyContent={"center"}
@@ -28,7 +42,13 @@ const Account = () => {
         <Heading>Sign in</Heading>
         <Field.Root invalid={false}>
           <Field.Label>Email</Field.Label>
-          <Input placeholder="me@example.com" />
+          <Input
+            placeholder="me@example.com"
+            {...register("email", {
+              required: true,
+              validate: (value) => validator.isEmail(value),
+            })}
+          />
           <Field.ErrorText>Invalid email</Field.ErrorText>
         </Field.Root>
         <Field.Root invalid={false}>
