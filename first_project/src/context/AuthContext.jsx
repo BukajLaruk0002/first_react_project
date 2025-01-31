a07e60226cb7590e998a612032187632b9f22816
jakub kural <maszpoczestujsie@wp.pl>
2025-01-31 14:12:05 +0100

    auth

---
+++ first_project/src/context/AuthContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem("jwtToken") || null);
+
+  const login = (newToken) => {
+    localStorage.setItem("jwtToken", newToken);
+    setToken(newToken);
+  };
+  const logout = () => {
+    localStorage.removeItem("jwtToken");
+    setToken(null);
+  };
+  return (
+    <AuthContext.Provider value={{ token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+export const useAuth = () => useContext(AuthContext);
